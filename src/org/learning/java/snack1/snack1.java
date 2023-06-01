a6900d59999b23e0b5e9c33b58e7e70a9ac5763d
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-06-01 14:56:27 +0200

    snack 1 end

--- src/org/learning/java/snack1/snack1.java
+++ src/org/learning/java/snack1/snack1.java
@@ -1,4 +1,18 @@
+//Snack1 Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
 package org.learning.java.snack1;
 
+import java.util.Scanner;
+
 public class snack1 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Inserisci un numero");
+    int n = scanner.nextInt();
+    if (n % 2 == 0) {
+      System.out.println(n);
+    } else {
+      System.out.println(n + 1);
+    }
+  }
 }
